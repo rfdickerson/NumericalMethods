8e884777ca47f400f1f3b21189960bc118be804e
Robert F. Dickerson <rfdickerson@us.ibm.com>
2015-10-15 21:59:40 -0400

    added some IO stuff just to learn

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 --
 -- Module      :  Main
--- Copyright   :  
+-- Copyright   :
 -- License     :  MIT License
 --
 -- Maintainer  : Robert F. Dickerson
@@ -12,9 +12,10 @@
 --
 -----------------------------------------------------------------------------
 
-
-
 module Main where
+
+import Data.List
+import System.IO
 
 -- | Acceleration due to gravity.
 gravityAccel :: Double
@@ -33,8 +34,9 @@ type Position = (Double, Double)
 type Force = (Double, Double)
 type Velocity = (Double, Double)
 type Vertex = (Double, Double)
+type Mass = Double
 
---data TriangleInfo = 
+--data TriangleInfo =
 --  Triangle {a :: Vertex, b :: Vertex, c :: Vertex}
 --    deriving (Show)
 
@@ -56,23 +58,28 @@ someTree = Node 5 (Node 3 Empty Empty) (Node 2 Empty Empty)
 
 -- search the binary tree for an element
 searchTree :: Eq a => a -> Tree a -> Bool
-searchTree e (Node c l r) = if c == e
-                               then True
-                            else
-                               searchTree e l || searchTree e r
-                                    
+searchTree e (Node c l r)
+    | c == e = True
+    | otherwise = searchTree e l || searchTree e r
+
+
 searchTree element Empty = False
-              
+
+hypotenuse l w = sqrt squaredH
+  where squaredH = l^2 +w^2
+
 
 -- areaTriangle :: Triangle -> Double
--- areaTriangle a = 0.5 * 
+-- areaTriangle a = 0.5 *
 
-data Particle
+data Ball
     =
-      Bead
+      Ball
+      !Mass -- mass
+      !Force -- force
       !Position
       !Velocity
-       deriving Show
+       deriving (Show, Eq)
 
 main :: IO ()
 main = putStrLn (printAngle (angle [3,7] [5,6]))
@@ -82,22 +89,28 @@ newtonRaphson guess f
   | difference <= epsilon = newguess
   | otherwise = newtonRaphson newguess f
   where
-    newguess = guess - f guess/fprime guess
+    newguess = guess - (f guess)/(fprime guess)
     difference = abs(newguess - guess)
-    fprime = deriv f
+    fprime = derivative f
 
 mysqrt :: Double -> Double -> Double
 mysqrt a x
   | difference <= epsilon = newguess
   | otherwise = mysqrt a newguess
   where
-    newguess = 1 / 2 * (x + a/x)
+    newguess = (1 / 2) * (x + a/x)
     difference = abs(newguess - x)
 
+-- returns an approximation of the derivative
+derivative :: (Double->Double) -> (Double->Double)
+derivative f = (\x -> (f (x + epsilon) - f x)/epsilon)
 
-deriv :: (RealFloat a) => (a->a) -> a -> a
+
+deriv :: (Double->Double) -> Double -> Double
 deriv f x = (f (x + dx) - f x) / dx
-    where dx = 0.0001*x
+    where dx = epsilon*x
+
+
 
 -- Some example functions that I don't use
 -- g :: (RealFloat a) => a -> a
@@ -106,35 +119,59 @@ deriv f x = (f (x + dx) - f x) / dx
 -- g2 :: (RealFloat a) => a -> a -> a
 -- g2 y x = x*y
 
--- Euler method
-euler :: (Double->Double) -> Double -> Double
-euler f y0 = y0 + h * f y0
-    where h = 0.1
-          
--- Backwards Euler --------------------
-eulerBackwards :: 
-	(Double->Double) -> 
-	Double -> 
-	Double
+type RealFunction = (Double->Double)
 
-eulerBackwards f y0 = newtonRaphson guess g
-  where 
-    h = 0.002
-    guess = euler f y0
+map2D :: RealFunction -> RealFunction -> (Double,Double) -> (Double, Double)
+map2D fx fy (x, y) = (fx x, fy y)
+
+-- update
+updateBall :: Ball -> Double -> Ball
+updateBall (Ball m f p v) dt = Ball m f p v
+  where
+    acceleration = force / m
+    force = 5
+
+-- view
+
+-- model
+initBall = Ball 5 (0,0) (0, 0) (1, 0)
+
+-- eulerIter
+-- differential equation, starting y, timestep
+eulerIter :: RealFunction -> Double -> Double -> [Double]
+eulerIter f h y0 =
+  let it y = eulerBackwards f h y
+  in
+    iterate it y0
+
+-- Euler method
+euler :: RealFunction -> Double -> Double -> Double
+euler f h y0 = y0 + h * f y0
+
+-- Backwards Euler --------------------
+eulerBackwards ::
+    (Double->Double) ->
+    Double ->
+    Double ->
+    Double
+
+eulerBackwards f h y0 = newtonRaphson guess g
+  where
+    guess = euler f h y0
     g = (\x -> x - y0 - h*f x)
 
 
 infinitelist = 1.0 : map (+ 0.01) infinitelist
 -- function, x0, y0
 
-eulerIter :: (Double->Double) -> Double -> [Double]
-eulerIter f y0 = y0 : eulerIter f yp 
-  where 
-    yp = eulerBackwards f y0 
+--eulerIter :: (Double->Double) -> Double -> [Double]
+--eulerIter f y0 = y0 : eulerIter f yp
+--  where
+--    yp = eulerBackwards f y0
 
 {-eulerIter :: (Double->Double) -> Double -> Double -> [(Double,Double)]
 eulerIter f x0 y0 = x
-  where 
+  where
     dx = 0.001
     yp = euler f y0
     x = 1.0 : map (+ dx) x
@@ -173,8 +210,19 @@ angle a b = case dot a b of
   Just r -> Just( acos (r / (mag a * mag b)))
   Nothing -> Nothing
 
- 
+say :: String -> IO ()
+say x = putStrLn $ "Buy " ++ x
 
-  -- gprime :: (RealFloat a) => a -> a
+say2 :: String -> IO Bool
+say2 x = do
+  putStrLn x
+  return True
+
+groceries :: [String]
+groceries = ["ham","tomato","garlic"]
+
+buyItem :: [String] -> String -> [String]
+buyItem list item = [item] ++ list
+
+-- gprime :: (RealFloat a) => a -> a
 -- gprime x = 3*x^2 - 4
-
